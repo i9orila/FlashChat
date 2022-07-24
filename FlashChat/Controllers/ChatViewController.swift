c5d9f298cf0077835154cd8c18c1742ed8f8e913
I9orila <i9orila@aol.com>
2022-07-24 18:23:32 +0300

    add Cloud Firestore save data and xib. file message cell

--- FlashChat/Controllers/ChatViewController.swift
+++ FlashChat/Controllers/ChatViewController.swift
@@ -8,33 +8,72 @@
 import UIKit
 import FirebaseAuth
 import FirebaseCore
+import FirebaseFirestore
 
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hi!"),
+        Message(sender: "1@3.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "Whats Up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
         
-
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
+        }
+    func LoadMessages() {
+        
     }
     
+    
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender =  Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                                                                      K.FStore.bodyField: messageBody]) { (error) in
+                if let e = error {
+                    print("Issue saving data to Firestore, \(e)")
+                } else {
+                    print("Succesfully saved data.")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         do {
-      try Auth.auth().signOut()
-        navigationController?.popToRootViewController(animated: true)
-    } catch let signOutError as NSError {
-      print("Error signing out: %@", signOutError)
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
-      
+}
+
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
 }
- 
+
---
+++ FlashChat/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by I9orila T on 21.07.2022.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String // mext message
+}
